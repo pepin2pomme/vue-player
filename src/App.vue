fd0d0df9f4f21da931dd2fc76264083dd3a540bd
milo <milosorelcpt@gmail.com>
2026-03-17 00:04:04 +0100

    feat: initialize videojs player with props and memory cleanup

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,9 @@
 <script setup>
-import videojs from 'video.js'
-import 'video.js/dist/video-js.css'
-import { Play, Pause, RotateCcw } from 'lucide-vue-next'
 
+import player from './components/player.vue';
 </script>
 
 <template>
-  <p>Bonsoir</p>
+  <p>Lecteur Video</p>
+  <player></player>
 </template>
---
+++ src/components/player.vue
@@ -0,0 +1,40 @@
+<script setup>
+import { ref, onMounted, onBeforeUnmount } from 'vue'
+import videojs from 'video.js'
+import 'video.js/dist/video-js.css'
+
+const lecteur = ref(null)
+const instancePlayer = ref(null)
+
+onMounted(() => {
+  instancePlayer.value = videojs(lecteur.value, {
+    controls: true,
+    autoplay: false,
+    preload: 'auto',
+    sources: [
+      {
+        src: 'https://vjs.zencdn.net/v/oceans.mp4',
+        type: 'video/mp4'
+      }
+    ]
+  })
+
+  
+})
+
+onBeforeUnmount(() => {
+  if(instancePlayer.value){
+    instancePlayer.value.dispose()
+  }
+})
+</script>
+
+<template>
+  <div class="player-container">
+    <p>Video</p>
+    <video 
+      ref="lecteur" 
+      class="video-js vjs-big-play-centered"
+    ></video>
+  </div>
+</template>
